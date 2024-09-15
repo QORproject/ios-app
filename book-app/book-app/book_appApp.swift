61cacc0357b1144975a0c814251f23afa03ee28b
hamadayuuki <yuukirinnma@gmail.com>
2024-09-15 11:50:21 +0900

    Init project

---
+++ book-app/book-app/book_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  book_appApp.swift
+//  book-app
+//
+//  Created by 濵田　悠樹 on 2024/09/15.
+//
+
+import SwiftUI
+
+@main
+struct book_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
